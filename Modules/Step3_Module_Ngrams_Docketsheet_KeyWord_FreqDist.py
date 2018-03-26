25c80071bf405c2e32f4132d13544a697c86e88a
Born2Ride <ccirelli2@gmail.com>
2018-03-26 08:11:16 -0400

    1.) Stage3:  Revised the code to now take Ngrams. 2.) Stage3 Module:  Create a stage 3 Module to accomodate some of the unique functions being used in Stage 3

--- Modules/Step3_Module_Ngrams_Docketsheet_KeyWord_FreqDist.py
+++ Modules/Step3_Module_Ngrams_Docketsheet_KeyWord_FreqDist.py
@@ -28,42 +28,6 @@ def import_docket_sheet_file(Excel_file):
     return df_docket_sheets_fit[TimePeriod_notNone]
 
 
-# CONCATENATE DOCKET SHEET TEXT
-
-def create_Concatenated_text_file(df_docketFile, new_file_name):
-    '''
-    Input      = doketFile as a dataframe. 
-    Operations = Create new file, limit docketfile to 'docket text', iterate rows, write to new file. 
-    Output     = Function will write the new file to the current-working-direcotry.  No other return from function.  
-    '''
-    
-    # Create new write file
-    New_File = open(str(new_file_name) + '.txt','w')
-    
-    # Limit Docket Sheet to Text Column
-    df_docket_text = df_docketFile['docket_text']
-    
-    # Create Loop Through List of Directories
-    for row in df_docket_text:
-        
-        # Write files to new file
-        New_File.write(str(row))
-        New_File.write('\n')
-    # Close File
-    New_File.close()
-    print('Concatenated text created\n')
-    return None
-
-
-# IMPORT CONCATENATED TEXT FILE
-
-def import_concatTxt(filename):
-    # Import Concat Text File
-    File = filename
-    File_open = open(File) 
-    print('Concatenated text imported\n')
-    return File_open.read()
-
 # CREATE A SET OF ALL HUMAN NAMES FROM THE NLTK CORPUS LIBRARY
 
 def get_set_human_names():
@@ -160,6 +124,74 @@ def get_docketsheet_ngrams(Tokenized_text):
     # Return to the user the Ngram_text
     return Ngrams_text
 
+
+
+# CODE TO OBTAIN KEY WORD APPEARANCE FOR DOCKETSHEETS 
+
+def get_KeyWordAppearance_DocketsheetEntries(Docketsheet, List_DictKeyWords):
+    '''
+    Purpose     = The purpose of this code is to generate a dataframe whose rows are the docketsheet entries, columns
+                  the key ngrams and the values the appearance of these ngrams in each docketsheet entry. 
+    Input       = 1.) A dataframe representing the docketsheet limited to only those applicable rows and columns. 
+                  2.) A single list of key words.  You may input a single list or loop over multiple lists of key words. 
+                      The important point is that the object must be input as a list. 
+    Operations  = 1.) Create a dataframe to house the rows and columns 
+                  2.) Since the names of each docketsheet entry do not indicate their position in the dataframe, create
+                      and object to keep the count of the num of rows that we iterate over, which will later be used in
+                      the naming of the rows in the new dataframe returned to the user. 
+                  3.) Create a list to capture the values 0/1 that will represent the appearance or not of each of the
+                      Ngrams.  This list is the row of values for each docketsheet entry. 
+                  4.) Clean and tokenize the text in the same manner as was done in Stage1. 
+                  5.) Create Ngrams of the docketsheet rows in the same manner as was done in Stage1. 
+                  6.) Iterate over each Ngram in the List of Ngrams input into this function. 
+                  7.) Determine if the Ngram is present and add values to the List_word_matches defined earlier .
+                  8.) Define our row. 
+                  9.) Append the row to our dataframe. 
+    
+    '''
+    
+    # Create New Dataframe
+    df_DAT = pd.DataFrame({}, index = List_DictKeyWords)
+    
+    # Count of Rows
+    Count = 0
+    
+    # Iterate over row of the docketsheet df as an enumerated tuple. 
+    for row in Docketsheet.itertuples():
+
+        # Star the Count of the rows
+        Count += 1
+        
+        # List to capture each row.  List will reset on each iteration of the code. 
+        List_word_matches_single_row = []
+        
+        # Clean row[4], which is the text column of the docketsheet. 
+        clean_tokenize_row = clean_andTokenize_text(row[4])                
+            
+        # Once the text of the row is tokenized, we need to create the Ngrams
+        docketsheet_ngrams = get_docketsheet_ngrams(clean_tokenize_row)
+            
+        # So iterate every word in key_word_list for each rows 
+        for Ngram in List_DictKeyWords:
+            
+            # Check to see if the word is in the clean text
+            if Ngram in docketsheet_ngrams:
+                # If there is a match, append 1 to the list
+                List_word_matches_single_row.append(1)
+            else: 
+                # If not, append 0
+                List_word_matches_single_row.append(0)
+        
+        # Create a string from the counter to add to your column name. 
+        Row_num = 'row' + str(Count) + ' ' 
+        
+        # Create a column in your new datafrmae to capture the case number and list of matches. 
+        df_DAT[Row_num + row[1] + ' ' + str(row[2])] = List_word_matches_single_row        
+
+        # Return our dataframe w/ an index as the list of key words, columns as each case. 
+    
+    return df_DAT
+            
 
 # FUNCTION TO CREATE AN NGRAM COLUMN
     
